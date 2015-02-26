25504bcc9bd9e4ec9a59a72132765118058ff49d
marcolino <marcosolari@gmail.com>
2015-02-26 18:00:38 +0100

    Header filter dropdown menu template

--- app/scripts/controllers/authentication.js
+++ app/scripts/controllers/authentication.js
@@ -4,13 +4,15 @@ app.controller('AuthenticationController',
   function ($scope, $rootScope, $location, cfg, Authentication, Countries) {
     $scope.cfg = cfg;
     $scope.countries = Countries;
-//$scope.countries = { 'ad': 'Andorra', 'it': 'Italy', };
     console.info('AUTH CTRL start, countries:', $scope.countries);
     var countryCode = '';
     $scope.filter = {
       voteMin: Math.ceil((cfg.person.vote.min + cfg.person.vote.max) / 2),
       commentsCountMin: 0,
+      country: '',
+      countryN: 0,
       nationality: {
+        countryN: 0, // TODO: -1 ?
         countryCode: countryCode,
         countryName: $scope.countries[countryCode],
       },
@@ -98,17 +100,28 @@ console.info('TODO: SEARCHING...');
     };
 
     $scope.activeCountries = function () {
-      return {
-        'it': 'Italy',
-        'ru': 'Russia',
-      };
+console.log('$scope.activeCountries called');
+      return [
+        { 'n': 1, 'code': 'it', 'name': 'Italy' },
+        { 'n': 2, 'code': 'ru', 'name': 'Russia' },
+      ];
+    };
+
+    $scope.setFilterCountry = function (n) {
+      var ac = $scope.activeCountries();
+      var max = ac.length;
+      $scope.filter.countryN = Math.max(0, Math.min(max, $scope.filter.countryN + n));
+console.log('$scope.setFilterCountry()', $scope.filter.countryN, ' => ', ac[$scope.filter.countryN]);
+      $scope.filter.country = ac[$scope.filter.countryN].name;
+      //return ac[$scope.filter.countryN];
     };
 
     $scope.setFilterNationalityCountry = function (code) {
 console.log('$scope.setFilterNationalityCountry:', code);
       $scope.filter.nationality.countryCode = code;
       $scope.filter.nationality.countryName = $scope.countries[code];
-      closeNavbarCollapseMenu();
+$event.stopPropagation();
+      //closeNavbarCollapseMenu();
     };
 
     // TODO: this function is in person controller: hot to have only one instance?
@@ -117,7 +130,8 @@ console.log('$scope.setFilterNationalityCountry:', code);
     };
 
     function closeNavbarCollapseMenu () {
-      $('#navbar-collapse-0').trigger('click');
+console.info('TRIGGERING CLICK TO CLOSE COLLAPSE MENU');
+      $('.navbar-toggle').trigger('click');
     }
 
     function setCredentials (response) {
